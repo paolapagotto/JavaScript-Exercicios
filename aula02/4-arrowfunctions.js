1eb49ee3c591a90e87d8269cc4ed137b621e19ba
paolapagotto <paolapagotto@gmail.com>
2020-08-27 17:55:04 +0100

    arrow functions

---
+++ aula02/4-arrowfunctions.js
@@ -0,0 +1,24 @@
+// Arrow Functions
+
+// nos permitem escrever funções com uma sintaxe mais compacta
+
+//SOMAR
+function somar(a, b){
+    return a + b;
+}
+
+let somar = (a, b) => a + b;
+
+let dobro = a => a * 2;
+
+// O operador flecha substitui a palavra reservada function
+
+let saudar = () => 'Olá, mundo!';
+
+let dobroNumero = numero => numero * 2;
+
+let horaAtual = () => {
+    let data = new Date();
+    return data.getHours() + ';' + data.getMinutes();
+}
+
